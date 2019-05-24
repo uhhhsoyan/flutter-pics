68ca193fe26a2353a6f51aef4e98acc20a0b30e7
Eric Essoyan <eric.essoyan@gmail.com>
2019-05-23 17:34:19 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  var app = MaterialApp(
+    home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print('Hi there!');
+        },
+      ),
+      appBar: AppBar(
+        title: Text('Lets see some images!')
+      ),
+    ),
+  );
+  runApp(app);
+}
